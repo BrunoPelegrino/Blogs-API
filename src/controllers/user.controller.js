f7d2d024338cc0fd47f79b24fd4e5f006cc4702c
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-10-05 19:24:37 -0300

    req4 finalizado/ token gerado para new user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
 const userService = require('../services/userService');
 
 const createUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
   const user = await userService.createUser(displayName, email, password, image);
-  return res.status(201).json(user);
+  // return res.status(201).json(user);
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: { email: user.email } }, secret, jwtConfig);
+
+  res.status(201).json({ token });
 };
 
 module.exports = {
