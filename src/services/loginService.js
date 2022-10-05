96e67b372042050039c7678e882c501304c11daf
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-10-05 16:20:43 -0300

    reup test req 3

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,5 +1,5 @@
 const { User } = require('../models');
-// const { generateToken } = require('../utils/JWT');
+// const { generateToken } = require('../utils/JWT'); 
 
 const authService = (email) => User.findOne({ where: { email } });
 
