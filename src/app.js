76791500a3ecd79626ae2bc2fedb8484e4eff6e4
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-10-05 23:37:58 -0300

    req8/ post category feito

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginControler = require('./controllers/login.controller');
 const userControler = require('./controllers/user.controller');
+const categoryController = require('./controllers/category.controller');
 const { validateDisplayName, validateEmail,
     validatePassword } = require('./middlewares/createUser.validation');
 const { authToken } = require('./middlewares/JWT.validation');
@@ -17,6 +18,7 @@ validateEmail,
 userControler.createUser);
 app.get('/user', authToken, userControler.getUsers);
 app.get('/user/:id', authToken, userControler.getUsersById);
+app.post('/categories', authToken, categoryController.createCategory);
 // ...
 
 // É importante exportar a constante `app`,
